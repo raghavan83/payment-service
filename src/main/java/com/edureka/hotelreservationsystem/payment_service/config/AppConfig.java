75f3d74859ded34835fc33548247800230ad3ac9
Raghavan S <raghavan2,s@tcs.com>
2024-08-19 07:50:10 +0530

    AppConfig changes

---
+++ src/main/java/com/edureka/hotelreservationsystem/payment_service/config/AppConfig.java
@@ -0,0 +1,26 @@
+package com.edureka.hotelreservationsystem.payment_service.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfig {
+
+	public AppConfig() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+//	@Bean
+//    public Logger.Level feignLoggerLevel() {
+//        return Logger.Level.FULL;
+//    }
+
+}
